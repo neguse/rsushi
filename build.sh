21dd220b3fcbc240d7ff8be94e881f2b9768faac
neguse <negcee@gmail.com>
2021-11-18 00:14:49 +0900

    install go

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# install go
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.3.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# copy wasm_exec.js
 cp $(go env GOROOT)/misc/wasm/wasm_exec.js web/
 
+# build
 export GOOS=js
 export GOARCH=wasm
 go build -o web/rsushi.wasm github.com/neguse/rsushi
