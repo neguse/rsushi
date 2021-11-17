f6e9c142f90679cf48e335ce5b41b4be74666d87
neguse <negcee@gmail.com>
2021-11-18 00:17:35 +0900

    curl

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # install go
+curl https://golang.org/dl/go1.17.3.linux-amd64.tar.gz -o go1.17.3.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
